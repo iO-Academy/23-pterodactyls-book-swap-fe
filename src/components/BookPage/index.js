a55df68b58de33bdae57e4b4dc3e516c64bf5d77
Fintan Hardy <fintan.hardy@gmail.com>
2023-08-21 15:02:00 +0100

    fetch json data and put in useState

--- src/components/BookPage/index.js
+++ src/components/BookPage/index.js
@@ -1,16 +1,42 @@
 import { useParams } from "react-router-dom";
 import "./book-page.css";
 import placeholder from "./placeholder.png";
+import { useEffect, useState } from "react";
 
 function BookPage() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [pages, setPages] = useState(0);
+  const [Genre, setGenre] = useState("");
+  const [blurb, setBlurb] = useState("");
+
+  const [reviews, setReviews] = useState([]);
+  const [reviewsNum, setReviewsNum] = useState(0);
+  const [reviewsAvg, setReviewsAvg] = useState(0);
+
   const { id } = useParams("");
 
-  return (
-    <div>
-      <h1>hello</h1>
-      <img src={placeholder}></img>
-    </div>
-  );
+  useEffect(() => {
+    fetch("https://book-swap-api.dev.io-academy.uk/api/books/" + { id })
+      .then((res) => res.json())
+      .then((info) => {
+        setTitle(info.data.title);
+        setAuthor(info.data.author);
+        setPages(info.data.page_count);
+        setGenre(info.data.genre.name);
+        setBlurb(info.data.blurb);
+        setReviews(info.data.reviews);
+      })
+      .catch((error) => console.error("Error fetching data", error));
+  }, []);
+
+  useEffect(() => {
+    const totalRating = reviews.reduce((sum, review) => sum + review.rating, 0);
+    setReviewsAvg(totalRating / reviews.length || 0); // avoid dividing by zero
+    setReviewsNum(reviews.length);
+  });
+
+  return <></>;
 }
 
 export default BookPage;
