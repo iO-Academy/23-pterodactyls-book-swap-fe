15f25b6fb5ed8466aba6fa47b72f1b1f64e6831d
Fintan Hardy <fintan.hardy@gmail.com>
2023-08-21 14:06:40 +0100

    fixed params error

--- src/components/BookPage/index.js
+++ src/components/BookPage/index.js
@@ -1,19 +1,15 @@
-import './book-page.css'
-import placeholder from './placeholder.png'
-
+import { useParams } from "react-router-dom";
+import "./book-page.css";
+import placeholder from "./placeholder.png";
 
 function BookPage() {
-
-  const{id} = useParams
-  
-  return(
-
+  const { id } = useParams("");
+  return (
     <div>
       <h1>hello</h1>
       <img src={placeholder}></img>
     </div>
-
-  )
+  );
 }
 
 export default BookPage;
