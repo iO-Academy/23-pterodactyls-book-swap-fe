0de817fb1258488fcbac6bd9aff843ad5acef086
Fintan Hardy <fintan.hardy@gmail.com>
2023-08-21 12:11:25 +0100

    added context file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Main from "./components/Main";
 import Nav from "./components/Nav";
+import BookContext from "./book-context";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Main />
-        <Nav />
+        <BookContext.Provider value={{}}>
+          <Main />
+          <Nav />
+        </BookContext.Provider>
       </BrowserRouter>
     </div>
   );
