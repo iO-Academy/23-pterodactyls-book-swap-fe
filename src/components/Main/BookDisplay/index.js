4208c1ab8cbf5f1935563c6d3603e4d14ded03ec
James Bell <jamesbell1138@gmail.com>
2023-08-22 11:47:34 +0100

    justustments made in bookdisplay.css

--- src/components/Main/BookDisplay/index.js
+++ src/components/Main/BookDisplay/index.js
@@ -15,19 +15,21 @@ function BookDisplay() {
     }, [])
     return (
         <div className='flex_container'>
-                {books.map((book, index) => (
-                    <Link key={index} to={`/Book/${book.id}`}>
+            {books.map((book, index) => (
+                <Link key={index} to={`/Book/${book.id}`}>
                     <div className='card'>
-                      <img src={book.image} alt={book.title} className='book_image' />
-                      <p className='book_title'>{book.title}</p>
-                      <p className='book_author'>{book.author}</p>
-                      <p className='book_genre'>{book.genre.name}</p>
+                        <img src={book.image} alt={book.title} className='book_image' />
+                        <div className='text_container'>
+                            <p className='book_title'>{book.title}</p>
+                            <p className='book_author'>{book.author}</p>
+                            <p className='book_genre'>{book.genre.name}</p>
+                        </div>
                     </div>
-                  </Link>
-                ))}
+                </Link>
+            ))}
         </div>
     )
 
 }
 
-    export default BookDisplay
+export default BookDisplay
