e91010eeee8db8258bc398678171f27d73222d65
Ben Peake <dearbenp@gmail.com>
2023-08-24 08:36:36 +0100

    genre filter working apart from dropdown displaying correct genre

--- src/components/GenreFilter/index.js
+++ src/components/GenreFilter/index.js
@@ -23,7 +23,8 @@ function GenreFilter(props) {
     <div className='container'>
         <label htmlFor='genreId'>Filter by genre:</label>
         <select className='dropdown' id='genreId' value='genreId' onChange={setGenre}>
-            <option className='dropdown'>All</option>
+        <option className='dropdown'>Select...</option>
+            <option className='dropdown'value={0}>All</option>
             {bookGenres.map(genre => 
         <option className='dropdown' value={genre.id} key={genre.id}>{genre.name}</option>)}
         </select>
