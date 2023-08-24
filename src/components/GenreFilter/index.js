ac2cc20f85fb3de1df59cc25775c220972525842
Ben Peake <dearbenp@gmail.com>
2023-08-24 09:33:09 +0100

    working genre filter

--- src/components/GenreFilter/index.js
+++ src/components/GenreFilter/index.js
@@ -22,7 +22,7 @@ function GenreFilter(props) {
  return(
     <div className='container'>
         <label htmlFor='genreId'>Filter by genre:</label>
-        <select className='dropdown' id='genreId' value='genreId' onChange={setGenre}>
+        <select className='dropdown' id='genreId' onChange={setGenre}>
         <option className='dropdown'>Select...</option>
             <option className='dropdown'value={0}>All</option>
             {bookGenres.map(genre => 
